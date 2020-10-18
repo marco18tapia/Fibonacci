93c4263b0c8ab18e5024310819e81511861a8150
marco18tapia <31663113+marco18tapia@users.noreply.github.com>
2020-10-18 18:35:50 -0500

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+
+package fibonacci;
+/**
+ *
+ * @author marco
+ */
+public class Fibonacci {
+
+    public static void main(String[] args) {
+       CalculoFibonacci calculofibonacci = new CalculoFibonacci();
+       calculofibonacci.mostrarFibonacci();         
+    }
+}
